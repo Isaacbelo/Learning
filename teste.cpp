c89dbd6fbed6fef6fa9101ef779518b72e185e9e
Isaac Belo <40142866+Isaacbelo@users.noreply.github.com>
2019-07-02 11:10:17 -0300

    Add files via upload

---
+++ teste.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    
+    for(i=0; i<50; i++)
+    cout<<'*';
+    
+    cout << endl;
+    
+    cout <<":::::::ESTUDOS EM C++:::::::";
+    
+    cout <<endl;
+    
+    for(i=0; i<50; i++)
+        cout<<"*";
+
+    return 0;
+}
